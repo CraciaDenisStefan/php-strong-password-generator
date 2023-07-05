3a37c597976159afecfc2bde927740f8a3c8c2a0
CraciDenisStefan <crdenis13@gmail.com>
2023-07-05 16:45:05 +0200

    milestone 2

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
-function generatePassword($length) {
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?';
-    $password = '';
-    $charactersLength = strlen($characters);
-
-   
-    for ($i = 0; $i <= $length; $i++) { 
-        $password .= $characters[rand(0, $charactersLength -1)];
-        
-    }
-
-    return $password;
-}
+include __DIR__. "/function.php";
 
 if (isset($_GET['length']) && $_GET['length'] !== '') {
     $passwordLength =trim($_GET['length']);
@@ -23,19 +11,32 @@ if (isset($_GET['length']) && $_GET['length'] !== '') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Password generate</title>
 </head>
 <body>
-<form class="mb-5 " action="index.php" method="GET">
+<div class="container text-white">
+    <div class="text-center my-5">
+        <h1 class="text-secondary">Strong Password Generator</h1>
+        <h3>Genera una password sicura</h3>
+    </div>
+    <div class="d-flex border justify-content-center">
+    <form class="my-5" action="index.php" method="GET">
         <div class="form-group">
             <label for="length">Lunghezza della password:</label>
             <input type="number" name="length" min="6" max="32" required>
             <div class="form-group m-2">
-                <button type="submit">invia</button>
+                <button class="btn btn-primary" type="submit">invia</button>
             </div>
         </div>
-</form>
+        <h4 class="my-5 border border-danger-subtle p-3"><?php if (isset($_GET['length']) && $_GET['length'] !== '') echo $generatedPassword ?></h4>
+    </form>
 
-<?php if (isset($_GET['length']) && $_GET['length'] !== '') echo $generatedPassword ?>
+    
+    </div>
+</div>
+
+
 </body>
 </html>
