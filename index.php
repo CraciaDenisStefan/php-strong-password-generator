244d2ed67c4f74be75c0ac51fe10c7c753336589
CraciDenisStefan <crdenis13@gmail.com>
2023-07-05 14:04:46 +0200

    [FEAT]Recupero lunghezza , ok.

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+if (isset($_GET['length']) && $_GET['length'] !== '') {
 
+    $passwordLength =$_GET['length'];
+   
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +13,16 @@
     <title>Password generate</title>
 </head>
 <body>
-    
+<form class="mb-5 " action="index.php" method="GET">
+        <div class="form-group">
+            <label for="length">Lunghezza della password:</label>
+            <input type="number" name="length" min="8" max="32" required>
+            <div class="form-group m-2">
+                <button type="submit">invia</button>
+            </div>
+        </div>
+</form>
+
+<?php if (isset($_GET['length']) && $_GET['length'] !== '') echo $passwordLength ?>
 </body>
 </html>
